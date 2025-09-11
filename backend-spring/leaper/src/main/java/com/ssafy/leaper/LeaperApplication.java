59d9a2a47a4e45004d9a9cb2baa8695752f9f8a3
DESKTOP-RBNO6LS\SSAFY <dudhr17050@naver.com>
2025-09-11 11:16:39 +0900

    chore: 초기프로젝트 파일

---
+++ backend-spring/leaper/src/main/java/com/ssafy/leaper/LeaperApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.leaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LeaperApplication.class, args);
+  }
+
+}
---
+++ backend-spring/leaper/src/test/java/com/ssafy/leaper/LeaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.leaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
