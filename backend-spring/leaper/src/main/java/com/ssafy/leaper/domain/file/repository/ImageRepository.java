825b28eb2f7394c38614940336d55026225947a9
sjw <shinjw0131@gmail.com>
2025-09-13 18:01:44 +0900

    fix: 클래스명 ë(S13P21A302-88)³경 시 생성된 중복 파일 제거

--- backend-spring/leaper/src/main/java/com/ssafy/leaper/domain/file/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.leaper.domain.file.repository;
-
-import com.ssafy.leaper.domain.file.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<File, Integer> {
-}
