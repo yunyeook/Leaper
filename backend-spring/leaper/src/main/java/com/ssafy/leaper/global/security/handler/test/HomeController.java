706cc0b3ee2c6678da9fbfe54132bd02849d4a87
hyobin <dnlem5415@gmail.com>
2025-09-14 00:58:06 +0900

    feat(S13P21A302-97): Spring Security Client 작성 및 구글 연결

---
+++ backend-spring/leaper/src/main/java/com/ssafy/leaper/global/security/handler/test/HomeController.java
@@ -0,0 +1,26 @@
+package com.ssafy.leaper.global.security.handler.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/success")
+    public String success(@AuthenticationPrincipal OAuth2User oauth2User, Model model) {
+        if (oauth2User != null) {
+            model.addAttribute("name", oauth2User.getAttribute("name"));
+            model.addAttribute("email", oauth2User.getAttribute("email"));
+            model.addAttribute("provider", oauth2User.getAttribute("provider"));
+        }
+        return "success";
+    }
+}
